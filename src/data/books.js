1cb5443fdabab3f71426c28dae2fa0f37cd1302b
billkapes <kapes15@yahoo.com>
2025-04-19 02:43:33 -0500

    first commit

---
+++ src/data/books.js
@@ -0,0 +1,45 @@
+export const books = [
+    {
+      slug: "flight-level-heist",
+      title: "Flight Level Heist",
+      image: "/books/1.jpg",
+      description: "A rogue agent must stop a shadow organization before global chaos erupts.",
+      longDescription: `
+        When a covert intelligence operative stumbles upon a hidden directive buried in classified files, 
+        he uncovers a network of betrayal that threatens to bring down world governments. 
+        From the streets of Istanbul to the corridors of Washington D.C., he must race against time 
+        to stop an invisible enemy from igniting global conflict.
+      `,
+      buyLink: "https://example.com/shadow-directive",
+      link: "/books/flight-level-heist",
+    },
+    {
+        slug: "shadow-directive",
+        title: "The Shadow Directive",
+        image: "/books/2.jpg",
+        description: "A rogue agent must stop a shadow organization before global chaos erupts.",
+        longDescription: `
+          When a covert intelligence operative stumbles upon a hidden directive buried in classified files, 
+          he uncovers a network of betrayal that threatens to bring down world governments. 
+          From the streets of Istanbul to the corridors of Washington D.C., he must race against time 
+          to stop an invisible enemy from igniting global conflict.
+        `,
+        buyLink: "https://example.com/shadow-directive",
+        link: "/books/shadow-directive",
+      },
+      {
+        slug: "shadow-directive",
+        title: "The Shadow Directive",
+        image: "/books/3.jpg",
+        description: "A rogue agent must stop a shadow organization before global chaos erupts.",
+        longDescription: `
+          When a covert intelligence operative stumbles upon a hidden directive buried in classified files, 
+          he uncovers a network of betrayal that threatens to bring down world governments. 
+          From the streets of Istanbul to the corridors of Washington D.C., he must race against time 
+          to stop an invisible enemy from igniting global conflict.
+        `,
+        buyLink: "https://example.com/shadow-directive",
+        link: "/books/shadow-directive",
+      },
+  ];
+  
